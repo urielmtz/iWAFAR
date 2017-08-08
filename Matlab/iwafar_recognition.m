944f743779506420883f4aa75ec38c140c028e4e
Uriel Martinez <Uriel Martinez>
2017-08-08 18:05:09 +0100

    add comments to iwafar_recognition file

--- Matlab/iwafar_recognition.m
+++ Matlab/iwafar_recognition.m
@@ -5,3 +5,5 @@
 
 % EMPTY MATLAB PROGRAM
 % Recognition of activities will be performed here
+% Here, a Dynamic Bayesian Network will be used for recognition of walking
+% activity, gait phases and gait events, from wearable sensors.
