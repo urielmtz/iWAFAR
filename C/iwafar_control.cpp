72eb5e902c2aac2fb8acf712cdc2bf19e0a863dd
Uriel Martinez <Uriel Martinez>
2017-08-08 18:08:47 +0100

    add initial libraries to iwafar_control in C/C++ language.

--- C/iwafar_control.cpp
+++ C/iwafar_control.cpp
@@ -5,3 +5,14 @@
 
 // EMPTY C/C++ PROGRAM
 // High, mid and low level control will be performed here
+
+#include <iostream>
+
+using namespace std;
+
+int main(int arcg, char **argv)
+{
+
+	cout << "iWAFAR control in C/C++ language" << endl;
+	return 0;
+}
